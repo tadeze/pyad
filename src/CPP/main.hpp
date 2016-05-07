48e26ad54fc506fc2d0ec28e8050393d0612fe6e
tadesse <tadeze@live.com>
2016-05-07 01:14:17 -0700

    makepy modified and src added

---
+++ src/CPP/main.hpp
@@ -0,0 +1,24 @@
+/*
+ * main.hpp
+ *
+ *  Created on: May 5, 2015
+ *      Author: tadeze
+ */
+
+#ifndef MAIN_HPP_
+#define MAIN_HPP_
+//#include "RForest.hpp"
+#include "IsolationForest.hpp"
+#include "utility.hpp"
+//#include "cincl.hpp"
+//#include "convForest.hpp"
+/*
+void buildForest(Forest &iff, doubleframe* test_dt, const double alpha,int stopLimit,float rho,
+		std::string output_name,ntstringframe* metadata,bool savePathLength);
+void buildForestPy(Forest &iff, doubleframe* test_dt, const double alpha,int stopLimit,float rho);
+
+void saveScoreToFile(std::vector<double> &scores,
+		std::vector<std::vector<double> > &pathLength,
+		const ntstringframe* metadata, std::string fName,bool savePathLength);
+*/
+#endif /* MAIN_HPP_ */
---
+++ src/testCPP.sh
@@ -0,0 +1,5 @@
+cd CPP/test 
+make 
+./test_iforest
+cd ../../
+
