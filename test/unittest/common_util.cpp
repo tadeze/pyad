e37d75a8efacfddb5b1c327949c3df2d5fea527f
Tadesse Zemicheal <tadesse.habte@gmail.com>
2017-04-04 18:30:41 -0700

    pybind & source files added

---
+++ test/unittest/common_util.cpp
@@ -0,0 +1,10 @@
+//
+// Created by tadeze on 4/4/17.
+//
+
+#include "common_util.hpp"
+
+ std::string common::filename() {
+     std::string filename("/home/tadeze/projects/pyiForestCmake/test/unittest/synth2d.dt");
+    return filename;
+    }
