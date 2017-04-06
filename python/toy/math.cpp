c698f9aea6fd9c6c52082df576379e80cc823880
Tadesse Zemicheal <tadesse.habte@gmail.com>
2017-04-05 23:55:50 -0700

    py interface updated

---
+++ python/toy/math.cpp
@@ -0,0 +1,9 @@
+//
+// Created by tadeze on 4/5/17.
+//
+
+#include "math.hpp"
+
+//double Math::square(double x){
+//    return x*x;
+//}
---
+++ python/toy/math.hpp
@@ -0,0 +1,18 @@
+//
+// Created by tadeze on 4/5/17.
+//
+
+#ifndef PYIFOREST_MATH_HPP
+#define PYIFOREST_MATH_HPP
+
+
+class Math {
+public:
+    double square(double x){ return x*x;}
+    Math(){};
+    virtual ~Math(){};
+
+};
+
+
+#endif //PYIFOREST_MATH_HPP
