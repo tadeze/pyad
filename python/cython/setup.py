a4486bc3291870700935565b56dc1806143e6973
Tad seldovia <tadeze@live.com>
2017-08-02 15:24:48 -0700

    make file modified

--- python/cython/setup.py
+++ python/cython/setup.py
@@ -16,7 +16,7 @@ REQUIRES = ['numpy', 'cython']
 AUTHOR = "Tadesse Zemicheal"
 EMAIL = "tadesse.habte@gmail.com"
 
-#LICENSE = "Apache 2.0"
+LICENSE = "Apache 2.0"
 
 SRC_DIR = "../../cpp/"
 CYTH_DIR="./"
@@ -42,7 +42,7 @@ if __name__ == "__main__":
           author=AUTHOR,
           author_email=EMAIL,
           url=URL,
-          #license=LICENSE,
+          license=LICENSE,
           cmdclass={"build_ext": build_ext},
           ext_modules=EXTENSIONS
           )
