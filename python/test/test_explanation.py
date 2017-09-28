ad5dd4624f1f97458aa2f2f935e764b37ab33cca
Tadesse Zemicheal <tadesse.habte@gmail.com>
2017-09-28 11:50:22 -0700

    soure restructured

--- python/test/test_explanation.py
+++ python/test/test_explanation.py
@@ -1,16 +1,16 @@
 import pyad as pft
 import numpy as np
 
-dt = np.loadtxt('synthetic5d34.csv',skiprows=1,delimiter=',')
-ff = pft.IsolationForest(dt,ntree=10000,nsample=100)
+dt = np.random.randn(1000,3)
+ff = pft.IsolationForest(dt,ntree=10,nsample=100)
 sc = ff.score(dt)
 print sc[2]
 
 print ff.explanation(dt[2,:])
 print dt[2,:]
 print ("Check model save")
-ff.save("iforest.if")
-xf = pft.IsolationForest()
-xf.load("iforest.if")
-print xf.score(dt)[2],sc[2]
+#ff.save("iforest.if")
+#xf = pft.IsolationForest()
+#xf.load("iforest.if")
+#print xf.score(dt)[2],sc[2]
 
