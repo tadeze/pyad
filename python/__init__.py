51e144d8c86c5b9c3ae64293e96ac9ff2a942cd5
Tadesse Zemicheal <tadesse.habte@gmail.com>
2019-01-22 18:34:00 -0800

    fix'
    	new file:   CMakeLists.txt
    	new file:   MANIFEST.in
    	modified:   python/__init__.py
    	new file:   python/ad.pxd
    	modified:   setup.py

--- python/__init__.py
+++ python/__init__.py
@@ -1,3 +1,4 @@
 from .ad import IsolationForest, IsolationTree, IForest, RotationForest
+from .loda import Loda
 
-__all__ = ['IsolationForest', 'IsolationTree', 'IForest', 'RotationForest']
+__all__ = ['IsolationForest', 'IsolationTree', 'IForest', 'RotationForest', 'Loda']
